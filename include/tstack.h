99bdb2fd094308a0d6057dca62193234a6a59682
Edelweizznick <kolia3003@yahoo.com>
2026-04-18 09:48:13 +0300

    Update tstack.h
    Added a templated stack class with basic operations.

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,39 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T arr[size];
+  int top_idx;
+
+public:
+  TStack() : top_idx(-1) {}
+
+  void Push(const T& value) {
+    if (top_idx < size - 1) {
+      arr[++top_idx] = value;
+    }
+  }
+
+  T Pop() {
+    if (top_idx >= 0) {
+      return arr[top_idx--];
+    }
+    return T{};
+  }
+
+  T Top() const {
+    if (top_idx >= 0) {
+      return arr[top_idx];
+    }
+    return T{};
+  }
+
+  bool IsEmpty() const {
+    return top_idx == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
